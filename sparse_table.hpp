7cd45004660eab15035ab49445652bb2539f1b24
Youran Zheng <123805661+FFTotoro@users.noreply.github.com>
2025-04-24 21:09:40 +0800

    Update sparse_table.hpp

--- sparse_table.hpp
+++ sparse_table.hpp
@@ -7,16 +7,16 @@ namespace IAOI_lib{
     public:
       sparse_table(vector<T> a){
         int k=__lg(a.size());
-        s.resize(a.size(),vector<T>(k+1));
+        s.resize(k+1,vector<T>(a.size()));
         for(int i=0;i<a.size();i++)
-          s[i][0]=a[i];
+          s[0][i]=a[i];
         for(int i=1;i<=k;i++)
           for(int j=0;j+(1<<i)<=a.size();j++)
-            s[j][i]=op(s[j][i-1],s[j+(1<<i-1)][i-1]);
+            s[i][j]=op(s[i-1][j],s[i-1][j+(1<<i-1)]);
       }
       inline T query(int l,int r){
         int k=__lg(r-l+1);
-        return op(s[l][k],s[r-(1<<k)+1][k]);
+        return op(s[k][l],s[k][r-(1<<k)+1]);
       }
   };
 }
